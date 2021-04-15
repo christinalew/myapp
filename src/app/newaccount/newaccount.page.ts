a1323bdb997563e129c748da2aa83c3da8217739
Leslie Ceja <cejaleslie@gmail.com>
2021-04-15 17:59:51 -0500

    linked create account button to home page

--- src/app/newaccount/newaccount.page.ts
+++ src/app/newaccount/newaccount.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from'@angular/router'
 
 @Component({
   selector: 'app-newaccount',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewaccountPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
-
+  gotoHomePage() {
+    this.router.navigate(['/home']);
+  }
 }
