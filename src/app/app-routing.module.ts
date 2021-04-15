c8daf27e2d07d6bed1d25925e829ac365fc56dc0
Leslie Ceja <cejaleslie@gmail.com>
2021-04-15 17:07:18 -0500

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,6 +15,10 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'newaccount',
+    loadChildren: () => import('./newaccount/newaccount.module').then( m => m.NewaccountPageModule)
+  },
 ];
 
 @NgModule({
