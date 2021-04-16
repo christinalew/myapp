66f2864bf317410fcda09a7f111fcc5ea5ff033f
Leslie Ceja <cejaleslie@gmail.com>
2021-04-15 19:58:48 -0500

    created a data service

--- src/app/newlist/newlist.page.ts
+++ src/app/newlist/newlist.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from'@angular/router'
 
 @Component({
   selector: 'app-newlist',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewlistPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  gotoHomePage() {
+    this.router.navigate(['/home']);
+  }
 }
